7669cddf3f52f1b89f2b13a7ef26fe093253f38a
Phost <cucchiaraquentin@gmail.com>
2025-05-14 22:57:25 +0200

    feat(interface Systems):  add a super nice way to not iterate into all the entities each update by indexation (thanks Christopher)

--- src/types/ISystem.ts
+++ src/types/ISystem.ts
@@ -1,13 +1,21 @@
+import {IComponent} from "./IComponent.ts";
+
 export interface ISystem {
+    indexedEntities: Array<number>;
+
     /**
      * Called when the system is added to the ECS.
+     * @param entityComponents The components of the entity that was added.
+     * test if the entity has the components that the system needs
+     * thanks to Christoper for the idea about the indexation of the entities into the systems
      */
-    // onAdded(): void;
+    onAdded(entityComponents: IComponent[]): void;
 
     /**
      * Called when the system is removed from the ECS.
      */
-    // onRemoved(): void;
+
+    onRemoved(entityId: number): void;
 
     /**
      * Called every frame to update the system.
